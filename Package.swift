db2ccf2d2d9762e7d86ffbfcd983590f557c9fa9
Joe Hinkle <joehinkle11@gmail.com>
2021-05-02 08:43:15 -0600

    internal dependency name conflict fix

--- Package.swift
+++ Package.swift
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
-			name: "Internal"
+			name: "GetIPInternal"
 		),
         .target(
             name: "GetIP",
-            dependencies: ["Internal"]),
+            dependencies: ["GetIPInternal"]),
         .testTarget(
             name: "GetIPTests",
             dependencies: ["GetIP"]),
